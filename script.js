d56f9c78aa8919c423d52b3a0ec07291a28cb3d5
Utkarsh Katiyar <utkarsh_katiyar@outlook.com>
2024-10-14 20:43:21 +0530

    Done

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// script.js
+document.getElementById("greetBtn").addEventListener("click", function() {
+    document.getElementById("greeting").innerText = "Hello, Utkarsh! Welcome to your personal webpage!";
+});
